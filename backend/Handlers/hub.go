103ebee83e4815dc8250b8dac57de007b25a4b8f
Immanuel Dsouza <dsouzad681@gmail.com>
2024-10-06 23:09:29 +0530

    commiting hub

--- backend/Handlers/hub.go
+++ backend/Handlers/hub.go
@@ -9,3 +9,26 @@ type Hub struct{
 	unregister chan *model.Client;
 }
 
+
+//new instance of hub
+func NewHub() *Hub{
+	return &Hub{
+		register: make(chan *model.Client),
+		unregister: make(chan *model.Client),
+		clients: make(map[*model.Client]bool),
+	}
+}
+
+//exectues go routines to check incoming socket events
+func (hub *Hub) Run(){
+	for {
+		select {
+		case client:= <-hub.register:
+			HandleUserRegisterEvent(hub,client)
+
+			break;
+		case client := <-hub.unregister:
+			HandleUserDisconnectEvent(hub,client)
+		}
+	}
+}
